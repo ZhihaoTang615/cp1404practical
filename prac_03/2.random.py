cb3213516ad1cd2c14916c73a4dbac03642c4ddd
tang <zhihao.tang@my.jcu.edu.au>
2025-06-04 16:48:25 +0800

    Add to prac_03

---
+++ prac_03/2.random.py
@@ -0,0 +1,27 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# 5 and 20
+print(random.randrange(3, 10, 2))  # line 2
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# 3 and 9  3 5 7 9
+# Could line 2 have produced a 4?
+# Could not. Because the step is 2, 3 + 2 = 5
+print(random.uniform(2.5, 5.5))  # line 3
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# 2.5 and 5.5
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100))
+
+
+
+
+
+
+
+
