cdbcebf1a8660f0eb2a59365b20bbe1738435f46
tang <zhihao.tang@my.jcu.edu.au>
2025-06-12 15:44:52 +0800

    complete the function

---
+++ prac_04/4.list_exercises.py
@@ -0,0 +1,26 @@
+# Part 1: Basic List Operations
+
+numbers = []
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+
+# Part 2: Woefully Inadequate Security Checker
+
+usernames = [
+    'jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+    'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+    'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob'
+]
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
