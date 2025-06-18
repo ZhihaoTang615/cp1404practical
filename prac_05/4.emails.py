88186cf4560b0d75f1710688e89d3f2774ec50b6
tang <zhihao.tang@my.jcu.edu.au>
2025-06-18 17:25:14 +0800

    Extract and confirm names

--- prac_05/4.emails.py
+++ prac_05/4.emails.py
@@ -5,10 +5,14 @@ Actual:
 """
 
 email_to_name = {}
-
 email = input("Email: ")
 while email != "":
-    email_to_name[email] = ""
+    name = ' '.join(email.split('@')[0].split('.')).title()
+    confirm = input(f"Is your name {name}? (Y/n) ").lower()
+    if confirm != "" and confirm != "y":
+        name = input("Name: ")
+    email_to_name[email] = name
     email = input("Email: ")
+
 
 print(email_to_name)
