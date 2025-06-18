2e2f909c9457f81b3e77bfe713534b751f690f06
tang <zhihao.tang@my.jcu.edu.au>
2025-06-18 16:39:27 +0800

    Add dictionary with 10 hex colour codes

---
+++ prac_05/2.hex_colours.py
@@ -0,0 +1,13 @@
+COLOUR_NAME_TO_HEX = {
+    "AbsoluteZero":    "#0048ba",
+    "AcidGreen":       "#b0bf1a",
+    "AliceBlue":       "#f0f8ff",
+    "AlizarinCrimson": "#e32636",
+    "Amaranth":        "#e52b50",
+    "Amber":           "#ffbf00",
+    "Amethyst":        "#9966cc",
+    "AntiqueWhite":    "#faebd7",
+    "Aqua":            "#00ffff",
+    "Aquamarine":      "#7fffd4"
+}
+
